138dc2f5e6321b43fc57f96382446ff4d9d232ed
Jayme Menchaca-Higgins <jaymehiggins0690@gmail.com>
2026-06-01 14:05:22 -0500

    added health check

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from rockapi.views.health_check import health_check
 from rockapi.views import (
     register_user, login_user,
     TypeView, RockView
@@ -14,5 +15,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
+    path('health', health_check),
     path('admin/', admin.site.urls),
 ]
